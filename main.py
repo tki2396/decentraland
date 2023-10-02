8c0938bdf8146039f87de48488823a04c7c216a3
Tobi Ijose <ijosetobi@gmail.com>
2023-10-01 21:56:25 -0500

    tested saving to db

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from app.routers.actor_routes import router as actor_routes
 from app.routers.ai_routes import router as ai_routes
-
+from app.services.database import client
+from app.core.config import DATABASE_NAME
 
 app = FastAPI()
 
@@ -19,26 +18,13 @@ app.add_middleware(
 app.include_router(actor_routes)
 app.include_router(ai_routes)
 
-
-DATABASE_URL = "mongodb://localhost:27017"
-DATABASE_NAME = "activitypub"
-
-client: AsyncIOMotorClient = None
-
-
-def get_db() -> AsyncIOMotorDatabase:
-    return app.mongodb
-
-
 @app.on_event("startup")
 async def startup_db_client():
-    global client
-    client = AsyncIOMotorClient(DATABASE_URL)
-    app.mongodb = client[DATABASE_NAME]
+    if client is not None:
+        app.mongodb = client[DATABASE_NAME]
 
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
-    global client
-    if client:
+    if client is not None:
         client.close()
